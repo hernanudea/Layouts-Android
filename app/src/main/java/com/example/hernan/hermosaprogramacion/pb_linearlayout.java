948a1a98739239e3d04a8e620d7617340b9da33e
Hernan Velasquez <hernanudea@gmail.com>
2016-04-30 22:30:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hernan/hermosaprogramacion/pb_linearlayout.java
@@ -0,0 +1,13 @@
+package com.example.hernan.hermosaprogramacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pb_linearlayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pb_linearlayout);
+    }
+}
---
+++ app/src/main/java/com/example/hernan/hermosaprogramacion/pb_relative_layout.java
@@ -0,0 +1,13 @@
+package com.example.hernan.hermosaprogramacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pb_relative_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pb_relative_layout);
+    }
+}
